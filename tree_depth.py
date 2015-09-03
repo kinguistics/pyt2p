764cb7a920178f55b0618c75e62ba7cf18ddf3c4
Ed Thomas King <etking@corn27.stanford.edu>
2015-09-02 19:29:01 -0700

    restructure directories

--- tree_depth.py
+++
@@ -1,8 +0,0 @@
-def max_depth(node, the_tree):
-    if node == -1:
-        return 0
-
-    left_depth = max_depth(the_tree.children_left[node], the_tree) + 1
-    right_depth = max_depth(the_tree.children_right[node], the_tree) + 1
-
-    return max(left_depth, right_depth)
