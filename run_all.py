5811b0a03c89d07b59f48c3f491919bcc9ecbb56
Ed King <etking@DN800cb40a.stanford.edu>
2016-03-15 16:14:24 -0700

    minor change to barley script

--- run_all.py
+++ run_all.py
@@ -1,6 +1,9 @@
-import os
+import os,sys
 
-STRESS = ['unstressed','stressed','binarystress']
-
+#STRESS = ['unstressed','stressed','binarystress']
+'''
 for stress in STRESS[:1]:
     os.system('qsub -cwd -l longq=1 -N %s-stress ./barley.submit.sh --stress %s' % (stress, stress))
+'''
+
+os.system('qsub -cwd -l longq=1 -N %s-stress ./barley.submit.sh %s' % sys.argv)
