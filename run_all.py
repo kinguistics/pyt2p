1c4750cbbf547310022955dd2b289c7ce60f312e
Ed King <etking2@gmail.com>
2025-04-11 22:59:45 +0100

    Delete a bunch of Stanford-infra-specific stuff

--- run_all.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-
-#STRESS = ['unstressed','stressed','binarystress']
-'''
-for stress in STRESS[:1]:
-    os.system('qsub -cwd -l longq=1 -N %s-stress ./barley.submit.sh --stress %s' % (stress, stress))
-'''
-
-os.system('qsub -cwd -l longq=1 -N %s-stress ./barley.submit.sh %s' % sys.argv)
--- run_allowables_tests.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-os.system('qsub -cwd -l longq=1 -N test-allowables ./barley.submit.sh')
